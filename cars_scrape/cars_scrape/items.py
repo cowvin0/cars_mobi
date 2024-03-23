1a70672a1f0293200ae7d057b4ce5d5bd4ea3fbd
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-03-22 23:46:22 -0300

    creating car item

--- cars_scrape/cars_scrape/items.py
+++ cars_scrape/cars_scrape/items.py
@@ -7,4 +7,24 @@ import scrapy
 
 
 class CarsItem(scrapy.Item):
-    pass
+
+    car_price = scrapy.Field()
+    url = scrapy.Field()
+    motorizacao = scrapy.Field()
+    carroceria = scrapy.Field()
+    km_andado = scrapy.Field()
+    combustivel = scrapy.Field()
+    cambio = scrapy.Field()
+    cor = scrapy.Field()
+    ano = scrapy.Field()
+    marca_carro = scrapy.Field()
+    nome_carro = scrapy.Field()
+    altura = scrapy.Field()
+    largura = scrapy.Field()
+    comprimento = scrapy.Field()
+    peso = scrapy.Field()
+    tanque = scrapy.Field()
+    entre_eixos = scrapy.Field()
+    porta_malas = scrapy.Field()
+    ocupantes = scrapy.Field()
+    direcao = scrapy.Field()
