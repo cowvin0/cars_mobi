409cf828f0ac00effc3b7772a7d1ffa43dedaaec
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-03-23 12:14:31 -0300

    some pipeline process

--- cars_scrape/cars_scrape/pipelines.py
+++ cars_scrape/cars_scrape/pipelines.py
@@ -5,9 +5,16 @@
 
 
 # useful for handling different item types with a single interface
+import re
 from itemadapter import ItemAdapter
 
 
 class CarsScrapePipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Remove some weird keys from car_price
+        val_price = adapter.get('car_price')
+        adapter['car_price'] = float(re.search(r'\d+\.?\d*', val_price).group(0).replace('.', ''))
+
         return item
--- cars_scrape/cars_scrape/spiders/mobi_spider.py
+++ cars_scrape/cars_scrape/spiders/mobi_spider.py
@@ -1,7 +1,7 @@
 import scrapy
 import re
 from scrapy.http import Request
-# from items import CarsItem
+from ..items import CarsItem
 
 
 class CarsSpider(scrapy.Spider):
@@ -39,33 +39,34 @@ class CarsSpider(scrapy.Spider):
 
         yield {"items": possible_classes[0][0]}
 
-        yield Request(
-            url="https://www.mobiauto.com.br" + possible_classes[0][0],
-            meta=dict(
-                dont_redirect=True,
-                handle_httpstatus_list=[302, 308],
-                playwright=True,
-                playwright_include_page=True,
-                errback=self.errback
-            ),
-            callback=self.parse_auto_items
-        )
+        # yield Request(
+        #     url="https://www.mobiauto.com.br" + possible_classes[0][0],
+        #     meta=dict(
+        #         dont_redirect=True,
+        #         handle_httpstatus_list=[302, 308],
+        #         playwright=True,
+        #         playwright_include_page=True,
+        #         errback=self.errback
+        #     ),
+        #     callback=self.parse_auto_items
+        # )
 
-        # for url in possible_classes[0]:
-        #     yield Request(
-        #         url="https://www.mobiauto.com.br" + url,
-        #         meta=dict(
-        #             dont_redirect=True,
-        #             handle_httpstatus_list=[302, 308],
-        #             playwright=True,
-        #             playwright_include_page=True,
-        #             errback=self.errback
-        #         ),
-        #         callback=self.parse_auto_items
-        #     )
+        for url in possible_classes[0][0:11]:
+            yield Request(
+                url="https://www.mobiauto.com.br" + url,
+                meta=dict(
+                    dont_redirect=True,
+                    handle_httpstatus_list=[302, 308],
+                    playwright=True,
+                    playwright_include_page=True,
+                    errback=self.errback
+                ),
+                callback=self.parse_auto_items
+            )
 
     async def parse_auto_items(self, response):
         page = response.meta['playwright_page']
+        car_item = CarsItem()
 
         await page.evaluate(r'''
                             var elements = document.querySelectorAll('.MuiAccordionSummary-content.MuiAccordionSummary-contentGutters.css-17o5nyn, .MuiAccordionSummary-content.MuiAccordionSummary-contentGutters.mui-style-17o5nyn');
@@ -89,31 +90,55 @@ class CarsSpider(scrapy.Spider):
         }''')
 
         mecanica = car_details['Mecânica']
-        dimensao = car_details['Dimensão']
+        dimensao = car_details['Dimensões']
         all_previous_info = response.xpath('//div[@class="mui-style-1n2g6aq"]//text()').getall()
         all_previous_info = {all_previous_info[i]: all_previous_info[i + 1] for i in range(len(all_previous_info) - 1)}
         car_price = response.xpath('//p[@class="mui-style-h31tor"]/text()').get()
         url = response.url
-        motorizacao = re.search(r'Motorização([\d.]+)', mecanica)
         carroceria = all_previous_info['Carroceria']
         km_andado = all_previous_info['KM']
         combustivel = all_previous_info['Combustível']
         cambio = all_previous_info['Câmbio']
         cor = all_previous_info['Cor']
         ano = all_previous_info['Ano']
-        marca_carro = response.xpath('//h1[@class"mui-style-4ato1b"]//text()').getall()
-        marca_carro = marca_carro[0]
+        marca_carro = response.xpath('//h1[@class="mui-style-4ato1b"]//text()').getall()
         nome_carro = ''.join(marca_carro)
-        altura = re.search(r'Altura \(mm\)(\d+)', dimensao)
-        largura = re.search(r'Largura \(mm\)(\d+)', dimensao)
-        comprimento = re.search(r'Comprimento \(mm\)(\d+)', dimensao)
-        peso = re.search(r'Peso \(kg\)(\d+)', dimensao)
-        tanque = re.search(r'Tanque \(L\)(\d+)', dimensao)
-        entre_eixos = re.search(r'Entre-eixos \(mm\)(\d+)', dimensao)
-        porta_malas = re.search(r'Porta-Malas \(L\)(\d+)', dimensao)
-        ocupantes = re.search(r'Ocupantes(\d+)', dimensao)
-        direcao = re.search(r'Direção([A-ZÁÉÍÓÚÂÊÔÃÕÇ][a-záéíóúâêôãõç]+)', mecanica)
+        
+        dict_list = {
+            'motorizacao': re.search(r'Motorização([\d.]+)', mecanica),
+            'altura': re.search(r'Altura \(mm\)(\d+)', dimensao),
+            'largura': re.search(r'Largura \(mm\)(\d+)', dimensao),
+            'comprimento': re.search(r'Comprimento \(mm\)(\d+)', dimensao),
+            'peso': re.search(r'Peso \(kg\)(\d+)', dimensao),
+            'tanque': re.search(r'Tanque \(L\)(\d+)', dimensao),
+            'entre_eixos': re.search(r'Entre-eixos \(mm\)(\d+)', dimensao),
+            'porta_malas': re.search(r'Porta-Malas \(L\)(\d+)', dimensao),
+            "ocupantes": re.search(r'Ocupantes(\d+)', dimensao),
+            "direcao": re.search(r'Direção([A-ZÁÉÍÓÚÂÊÔÃÕÇ][a-záéíóúâêôãõç]+)', mecanica),
+        }
+
+        for var_name, value in dict_list.items():
+            if value:
+                item = value.group(1)
+                try:
+                    car_item[var_name] = float(item)
+                except:
+                    car_item[var_name] = item
+            else:
+                car_item[var_name] = value
+
+        car_item['car_price'] = car_price
+        car_item['url'] = url
+        car_item['carroceria'] = carroceria
+        car_item['km_andado'] = km_andado
+        car_item['combustivel'] = combustivel
+        car_item['cambio'] = cambio
+        car_item['cor'] = cor
+        car_item['ano'] = ano
+        car_item['marca_carro'] = marca_carro[0]
+        car_item['nome_carro'] = nome_carro
 
         await page.close()
 
-        yield {"items": car_details}
+        yield car_item
+        # yield {"items": car_details}
