a3ab7bf1f9e717ec60f5751a943d217478a51fab
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-04-07 15:43:20 -0300

    in the hood

--- scratch_models/linear_reg.py
+++ scratch_models/linear_reg.py
@@ -1,10 +1,41 @@
 import numpy as np
 import pandas as pd
+from functools import wraps
+from scipy.optimize import minimize
+
+data = (
+    pd.read_csv('https://raw.githubusercontent.com/m-clark/generalized-additive-models/master/data/pisasci2006.csv')
+    .drop(columns='Country')
+    .dropna()
+    .reset_index(drop=True)
+    )
+
 
 class LinearRegression:
-    def __init__(self, intercept=1):
-        self.intercept = intercept
+    def __init__(self, X, y):
+        self._cols = X.columns
+        self._n = X.shape
+        self.X = X.to_numpy()
+        self.y = y.to_numpy()
+
+    def estimate(self):
+        comp = np.linalg.inv(self.X.T @ self.X)
+        return comp @ self.X.T @ self.y
+
+
+    def loglik(self):
+        betas = self.estimate()
+        mat_dif = self.y - self.X @ betas
+        sigma = (mat_dif.T @ mat_dif) / (self._n[0] - betas.shape[0])
+        exp_ = np.exp(- (mat_dif.T @ mat_dif) / (2 * sigma))
+
+        return exp_ / ((2 * np.pi * sigma) ** (self._n / 2))
 
     def fit(self, X, y):
+        return self.batata
+        
 
-        estimate_beta = np.linalg.inv(np.dot(X.T, X))
+X = data.drop(columns='Overall')
+y = data.Overall
+
+reg = LinearRegression(X=X, y=y)
