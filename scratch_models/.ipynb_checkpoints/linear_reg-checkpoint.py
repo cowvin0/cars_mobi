1de90b55c92a627308040f5419149a47d2b6b72b
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-05-02 17:56:47 -0300

    bald code with some hair

---
+++ scratch_models/.ipynb_checkpoints/linear_reg-checkpoint.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy.stats import f, t
+import statsmodels.api as sm
+
+data = (
+    pd.read_csv(
+        "https://raw.githubusercontent.com/m-clark/generalized-additive-models/master/data/pisasci2006.csv"
+    )
+    .drop(columns="Country")
+    .dropna()
+    .reset_index(drop=True)
+)
+
+
+class LinearRegression:
+    def __init__(self, X, y, intercept=True):
+        if intercept:
+            self.X = (X.
+                      reindex(["C", *X.columns], axis=1).
+                      assign(C=1).
+                      to_numpy()
+                      )
+        else:
+            self.X = X.to_numpy()
+
+        self.intercept = intercept
+        self._cols = X.columns
+        self._n = self.X.shape
+        self.y = y.to_numpy()
+
+    def fit(self):
+        comp = np.linalg.inv(self.X.T @ self.X)
+        self.diag = comp
+        self.coefs = comp @ self.X.T @ self.y
+        self.residuals = self.y - np.dot(self.X, self.coefs)
+        self.sse = (self.residuals**2).sum()
+        self.df_res = self._n[0] - self.coefs.shape[0]
+        self.df_model = self._n[1]
+        self.sigma = np.sqrt(self.sse / self.df_res)
+        self.sst = (self.y**2).sum() - (
+            self.y.sum() ** 2 / self._n[0] if self.intercept else 0
+        )
+        self.mst = self.sst / (self._n[0] - 1)
+        self.ssr = self.sst - self.sse
+        self.msr = self.ssr / (
+            self.df_model - 1 if self.intercept else self._n[1]
+        )
+        self.fstatistic = self.msr / (self.sigma**2)
+        self.f_pvalue = 1 - f.cdf(self.fstatistic, self.df_model, self.df_res)
+        self.rsquared = self.ssr / self.sst
+        self.rsquared_adj = 1 - (self.sigma**2) / self.mst
+        self.coefs_var = self.sigma * np.sqrt(np.diag(comp))
+        self.tstatistic = self.coefs / self.coefs_var
+        self.t_pvalue = (1 - t.cdf(abs(self.tstatistic), self.df_res)) * 2
+        self._hat = self.X @ comp @ self.X.T
+        self.std_residuals = self.residuals / (
+            np.sqrt(self.sigma**2 * (1 - np.diag(self._hat)))
+        )
+        self.cook_distance = (self.std_residuals**2 * np.diag(self._hat)) / (
+            self._n[1] * (1 - np.diag(self._hat))
+        )
+        return self
+
+    def predict(self, X):
+        if self.intercept:
+            preds = (
+                X.reindex(["C", *self._cols], axis=1).assign(C=1).to_numpy()
+                @ self.coefs
+            )
+            return preds
+        else:
+            preds = X.reindex([*self._cols], axis=1).to_numpy() @ self.coefs
+            return preds
+
+    def diagnostics(self, X=None):
+        if X:
+            pass
+        else:
+            sm.qqplot(self.residuals, line="q")
+
+            # return sns.scatterplot(x=self.X @ self.coefs, y=self.residuals)
+
+
+X = data.drop(columns="Overall")
+y = data.Overall
+
+reg = LinearRegression(X=X, y=y, intercept=True).fit()
--- scratch_models/linear_reg.py
+++ scratch_models/linear_reg.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import statsmodels.api as sm
 from scipy.stats import f, t
+import matplotlib.pyplot as plt
 
 data = (
     pd.read_csv(
@@ -13,61 +15,124 @@ data = (
 )
 
 
-class LinearRegression:
-    def __init__(self, X, y, intercept=True):
+class BaseMetrics:
+    def __init__(self, X, y, intercept):
         if intercept:
-            self.X = X.reindex(["C", *X.columns], axis=1).assign(C=1).to_numpy()
+            self.X = np.insert(X, 0, 1, axis=1)
         else:
             self.X = X.to_numpy()
 
-        self.intercept = intercept
         self._cols = X.columns
+        self.intercept = intercept
         self._n = self.X.shape
         self.y = y.to_numpy()
+        self.comp = np.linalg.inv(self.X.T @ self.X)
 
-    def fit(self):
-        comp = np.linalg.inv(self.X.T @ self.X)
-        self.diag = comp
-        self.coefs = comp @ self.X.T @ self.y
-        self.residuals = self.y - np.dot(self.X, self.coefs)
-        self.sse = (self.residuals**2).sum()
-        self.df_res = self._n[0] - self.coefs.shape[0]
-        self.df_model = self._n[1]
-        self.sigma = np.sqrt(self.sse / self.df_res)
-        self.sst = (self.y**2).sum() - (
+    def coefficients(self):
+        return self.comp @ self.X.T @ self.y
+
+    def fitted(self):
+        return self.X @ self.coefficients()
+
+    def residuals(self):
+        return self.y - self.fitted()
+
+    def _ss_error(self):
+        return (self.residuals()**2).sum()
+
+    def _ss_total(self):
+        return (self.y**2).sum() - (
             self.y.sum() ** 2 / self._n[0] if self.intercept else 0
         )
-        self.mst = self.sst / (self._n[0] - 1)
-        self.ssr = self.sst - self.sse
-        self.msr = self.ssr / (self.df_model - 1 if self.intercept else self._n[1])
-        self.fstatistic = self.msr / (self.sigma**2)
-        self.f_pvalue = 1 - f.cdf(self.fstatistic, self.df_model, self.df_res)
-        self.rsquared = self.ssr / self.sst
-        self.rsquared_adj = 1 - (self.sigma**2) / self.mst
-        self.coefs_var = self.sigma * np.sqrt(np.diag(comp))
-        self.tstatistic = self.coefs / self.coefs_var
-        self.t_pvalue = (1 - t.cdf(abs(self.tstatistic), self.df_res)) * 2
-        self._hat = self.X @ comp @ self.X.T
-        self.std_residuals = self.residuals / (
-            np.sqrt(self.sigma**2 * (1 - np.diag(self._hat)))
+
+    def _ss_model(self):
+        return self._ss_total() - self._ss_error()
+
+    def df_error(self):
+        return self._n[0] - self.coefficients().shape[0]
+
+    def sigma(self):
+        return np.sqrt(self._ss_error() / self.df_error())
+
+    def _mse_total(self):
+        return self._ss_total() / (self._n[0] - 1)
+
+    def df_model(self):
+        return self._n[1] - 1 if self.intercept else self._n[1]
+
+    def _mse_model(self):
+        return self._ss_model() / self.df_model()
+
+    def f_value(self):
+        return self._mse_model() / (self.sigma() ** 2)
+
+    def f_pvalue(self):
+        return 1 - f.cdf(self.f_value(), self.df_model(), self.df_error())
+
+    def rsquared(self):
+        return self._ss_model() / self._ss_total()
+
+    def adj_rsquared(self):
+        return 1 - (self.sigma() ** 2) / self._mse_total()
+
+    def _coefficients_var(self):
+        return self.sigma() * np.sqrt(np.diag(self.comp))
+
+    def t_value(self):
+        return self.coefficients() / self._coefficients_var()
+
+    def t_pvalue(self):
+        return (1 - t.cdf(abs(self.t_value()), self.df_error())) * 2
+
+    def rstandard(self):
+        return self.residuals() / self.sigma()
+
+    def _hat(self):
+        return self.X @ self.comp @ self.X.T
+
+    def rstudent(self):
+        return self.residuals() / (
+            self.sigma() * np.sqrt(1 - np.diag(self._hat()))
         )
-        self.cook_distance = (self.std_residuals**2 * np.diag(self._hat)) / (
-            self._n[1] * (1 - np.diag(self._hat))
+
+    def _cook_distance(self):
+        return (self.rstudent() ** 2 * np.diag(self._hat())) / (
+            self._n[1] * (1 - np.diag(self._hat()))
         )
+
+
+class LinearRegression(BaseMetrics):
+    def __init__(self, X, y, intercept=True):
+        super().__init__(X, y, intercept)
+
+    def fit(self):
         return self
 
     def predict(self, X):
         if self.intercept:
             preds = (
                 X.reindex(["C", *self._cols], axis=1).assign(C=1).to_numpy()
-                @ self.coefs
+                @ self.coefficients()
             )
-            self.residuals = self.y - preds
             return preds
         else:
-            preds = X.reindex([*self._cols], axis=1).to_numpy() @ self.coefs
-            self.residuals = self.y - preds
+            preds = (
+                X.reindex([*self._cols], axis=1).to_numpy()
+                @ self.coefficients
+            )
             return preds
+
+    def diagnostics(self, X=None):
+        if X:
+            pass
+        else:
+            fig, ax = plt.subplots(2, 2, figsize=(12, 6))
+            ind = [*range(1, self._n[0] + 1)]
+            sns.scatterplot(x=self.X @ self.coefficients(), y=self.residuals(), ax=ax[0, 0])
+            ax[0, 0].axhline(0, color="red")
+            sm.qqplot(self.residuals(), line="q", ax=ax[1, 0])
+            sns.scatterplot(x=ind, y=self._cook_distance(), ax=ax[0, 1])
+            sns.scatterplot(x=ind, y=np.diag(self._hat()), ax=ax[1, 1])
 
 
 X = data.drop(columns="Overall")
